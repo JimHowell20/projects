ab46809ca4932763e56f3263c22490d554707b12
Jim Howell <howell20@msu.edu>
2015-02-05 02:04:26 -0500

    changed drawing to heptagon

--- Step1/ChildView.cpp
+++ Step1/ChildView.cpp
@@ -12,9 +12,9 @@
 #endif
 
 
+#define M_PI 3.14159265359
+
 // CChildView
-
-
 CChildView::CChildView()
 {
 	m_linefmx = 0.5;
@@ -55,43 +55,45 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 
 void CChildView::OnGLDraw(CDC * pDC)
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Simple rectangle example
 	int width, height;
 	GetSize(width, height);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, // left
-		1.0, // right
-		0.0, // bottom
-		GLdouble(height) / GLdouble(width), // top
-		1.0, // near
-		-1.0); // far
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, // left
+        1.0, // right
+        0.0, // bottom
+        GLdouble(height) / GLdouble(width), // top
+        1.0, // near
+        -1.0); // far
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glColor3d(1.0, 0., 0.);
+	glColor3d(0.5, 1.0, 0.0);
 	
-	glBegin(GL_POLYGON);
-	glVertex2d(0.25, 0.25);
-	glVertex2d(0.75, 0.25);
-	glVertex2d(0.75, 0.75);
-	glVertex2d(0.25, 0.75);
-	glEnd();
-
-
 	glBegin(GL_LINE_STRIP);
-	glVertex2d(0.1, 0.1);
-	glVertex2d(0.8, 0.22);
-	glVertex2d(0.78, 0.9);
-	glVertex2d(0.2, 0.9);
+	glVertex2d(0.256, 0.274);
+	glVertex2d(0.745, 0.274);
+	glVertex2d(0.745, 0.75);
+	glVertex2d(0.256, 0.75);
+	glVertex2d(0.256, 0.274);
+	glEnd();
+
+	glColor3d(1.0, 0.5, 0.0);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 7; ++i) {
+		glVertex2d(0.5 + 0.25*sin(i / 7.0 * 2 * M_PI),
+			0.5 + 0.25*cos(i / 7.0 * 2 * M_PI));
+	}
 	glEnd();
 	
-	glColor3d(0.0, 1.0, 0.0);
+
+	glColor3d(1.0, 1.0, 0.0);
 
 	glBegin(GL_LINES);
 	glVertex2d(m_linefmx, m_linefmy);
